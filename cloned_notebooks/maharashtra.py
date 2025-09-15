64735c7ff420f5c67bd9a3a2c5bba24833ec5c12
Gunturu uday kiran <168414475+gunturuudaykiran@users.noreply.github.com>
2025-09-15 16:03:22 +0530

    Auto-fix for task maharashtra

---
+++ cloned_notebooks/maharashtra.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT * FROM weather_catalog.weather_schema.weather_karnataka LIMIT 10;
+
+# COMMAND ----------
+
+df = spark.read.table('weather_catalog.weather_schema.weather_karnataka')
+display(df.limit(10))
